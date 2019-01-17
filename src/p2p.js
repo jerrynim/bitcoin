17aa84e58ea869bf7c97517647e8a93a025631ff
jerrynim <tjerry3@naver.com>
2019-01-17 20:01:02 +0900

    handle close&error

--- src/p2p.js
+++ src/p2p.js
@@ -6,12 +6,14 @@ const getSockets = () => sokets;
 
 const startP2PServer = (server) => {
   const wsServer = new WebSockets.Server({ server });
-  wsServer.on("connection", (ws) => console.log(`Hello Socket`));
+  wsServer.on("connection", (ws) => initSocketConnection(ws));
   console.log("P2P Server running");
 };
 
 const initSocketConnection = (socket) => {
   sockets.push(socket);
+  socket.on("message", (data) => console.log(data));
+  setTimeout(() => socket.send("Welcome"), 5000);
 };
 
 const connectToPeers = (newPeer) => {
@@ -19,6 +21,15 @@ const connectToPeers = (newPeer) => {
   ws.on("open", () => {
     initSocketConnection(ws);
   });
+};
+
+const handleSocketError = (ws) => {
+  const closeSocketConnection = (ws) => {
+    ws.close();
+    sockets.splice(sockets.indexOf(ws), 1);
+  };
+  ws.on("close", () => closeSocketConnection(ws));
+  ws.on("error", () => closeSocketConnection(ws));
 };
 
 module.exports = {
