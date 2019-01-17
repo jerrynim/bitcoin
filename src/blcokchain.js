850dffe268d54222802f45386331eb44ae9b942b
jerrynim <tjerry3@naver.com>
2019-01-17 13:14:00 +0900

    isNewBlockValid

--- src/blcokchain.js
+++ src/blcokchain.js
@@ -1,3 +1,5 @@
+const CryptoJS = require("crypto-js");
+
 class Block {
   constructor(index, hash, previousHash, timestamp, data) {
     this.index = index;
@@ -8,13 +10,55 @@ class Block {
   }
 }
 
+const genesisBlock = new Block(
+  0,
+  "2C4CEB90344F20CC4C77D626247AED3ED530C1AEE3E6E85AD494498B17414CAC",
+  null,
+  1520312194926,
+  "This is the genesis!!"
+);
+
 let blockchain = [genesisBlock];
+
+const getLastBlock = () => blockchain[blockchain.length - 1];
+const getTimestamp = () => Date().getTime() / 1000;
+const createHash = (index, previousHash, timestamp, data) =>
+  CryptoJS.SHA256(index + previousHash + timestamp + data).toString;
 
 const CreateNewBlock = (data) => {
   const previousBlock = getLastBlock();
   const newBlockIndex = previousBlock[blockchain.length + 1];
   const newTimestamp = getTimestamp();
+  const newHash = createHash(
+    newBlockIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock = new Block(
+    newBlockIndex,
+    newHash,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
 };
 
-const getLastBlock = () => blockchain[blockchain.length - 1];
-const getTimestamp = () => Date().getTime() / 1000;
+const getBlockHash = (block) =>
+  createHash(block.index, block.previousHash, block.timestamp, block.data);
+
+const isNewBlockValid = (candidateBlock, latestBlock) => {
+  if (latestBlock.index + 1 !== candidateBlock.index) {
+    console.log("The candidate block doesnt have a valid index");
+    return false;
+  } else if (latestBlock.hash !== candidateBlock.previousHash) {
+    console.log(
+      "The previousHash of the candidate block is not the hash of the latest block"
+    );
+    return false;
+  } else if (candidateBlock.hash !== getBlockHash(candidateBlock)) {
+    console.log("The hash of this block is invalid");
+    return false;
+  }
+  return true;
+};
