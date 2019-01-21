c245e4910194e90ed88c5bb2ea8b77e2e32fd172
jerrynim <tjerry3@naver.com>
2019-01-21 18:41:41 +0900

    tx In&Out strucutre valid check

--- src/transaction.js
+++ src/transaction.js
@@ -75,17 +75,92 @@ const updateUTxOuts = (newTxs, uTxOutList) => {
       });
     })
     .reduce((a, b) => a.concat(b), []);
+
+  const spentTxOuts = newTxs
+    .map((tx) => tx.txIns)
+    .reduce((a, b) => a.concat(b), [])
+    .map((txIn) => new UTxOut(txIn.txOutId, txIn.txOutindex, "", 0));
+
+  const resultingUTxOuts = uTxOutList
+    .filter((utx0) => !findUTxOut(utx0.txOutId, utx0.txOutIndex, spentTxOuts))
+    .concat(newUTxOuts);
+
+  return resultingUTxOuts;
+};
+// uTxOuts[A(40), B, C, D, E, F] >>>> A(40) >> ZZ(10), MM(30) >>> [B,C,D,E,F,ZZ,MM]
+
+const isTxInStructureValid = (txIn) => {
+  if (txIn === null) {
+    console.log("The txIn appears to be null");
+    return false;
+  } else if (typeof txIn.signature !== "string") {
+    console.log("The txIn doesn't have a valid signature");
+    return false;
+  } else if (typeof txIn.txOutId !== "string") {
+    console.log("The txIn doesn't have a valid txOutId");
+    return false;
+  } else if (typeof txIn.txOutIndex !== "number") {
+    console.log("The txIn doesn't have a valid txOutIndex");
+    return false;
+  } else {
+    return true;
+  }
 };
 
-const spentTxOuts = newTxs
-  .map((tx) => tx.txIns)
-  .reduce((a, b) => a.concat(b), [])
-  .map((txIn) => new UTxOut(txIn.txOutId, txIn.txOutindex, "", 0));
+const isAddressValid = (address) => {
+  if (address.length !== 130) {
+    console.log("The address length is not the expected one");
+    return false;
+  } else if (address.match("^[a-fA-F0-9]+$") === null) {
+    console.log("The address doesn't match the hex patter");
+    return false;
+  } else if (!address.startsWith("04")) {
+    console.log("The address doesn't start with 04");
+    return false;
+  } else {
+    return true;
+  }
+}; //"^[a-fA-F0-9]+$" 헥사 데시멀 정규코드
 
-const resultingUTxOuts = uTxOutList
-  .filter((utx0) => !findUTxOut(utx0.txOutId, utx0.txOutIndex, spentTxOuts))
-  .concat(newUTxOuts);
+const isTxOutStructureValid = (txOut) => {
+  if (txOut === null) {
+    console.log("The txOut appears to be null");
+    return false;
+  } else if (txOut.address !== "string") {
+    console.log("The txOut doesn't have a valid string as address");
+    return false;
+  } else if (!isAddressValid(txOut.address)) {
+    console.log("The txOut doesn't have a valid address");
+    return false;
+  } else if (txOut.amount !== "number") {
+    console.log("The txOut doesn't have a valid amount");
+    return false;
+  } else {
+    return true;
+  }
+};
 
-return resultingUTxOuts;
-
-// uTxOuts[A(40), B, C, D, E, F] >>>> A(40) >> ZZ(10), MM(30) >>> [B,C,D,E,F,ZZ,MM]
+const isTxStructureValid = (tx) => {
+  if (typeof tx.id !== "string") {
+    console.log("Tx ID is not valid");
+    return false;
+  } else if (!(tx.txIns instanceof Array)) {
+    console.log("The txIns are not an array");
+    return false;
+  } else if (
+    !tx.txIns.map(!isTxInStructureValid).reduce((a, b) => a && b, true)
+  ) {
+    console.log("The structure of one of the txIn is not valid");
+    return false;
+  } else if (!(tx.txOuts instanceof Array)) {
+    console.log("The txOuts are not an array");
+    return false;
+  } else if (
+    !tx.txOuts.map(isTxOutStructureValid).reduce((a, b) => a && b, true)
+  ) {
+    console.log("The structure of one of the txOut is not valid");
+    return false;
+  } else {
+    return true;
+  }
+};
